9bf9e61709e47a4d079d0bb214eaf763a37d0827
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-07-19 16:56:04 +0300

    curs39 crud operations

--- src/main/java/tests/RestExample.java
+++ src/main/java/tests/RestExample.java
@@ -2,7 +2,13 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
 
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
@@ -11,19 +17,37 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class RestExample {
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void restExampleTest() {
 		
+		Faker fake = new Faker();
+		
+		JSONObject requestBody = new JSONObject();
+		requestBody.put( "title", fake.beer().name());
+		requestBody.put("body", fake.chuckNorris().fact());
+		
+		File fisierJson = new File("data.json");
+		
 		Response response = 
 				given().
-					header("Content-Type", "application/json"). 
-					body("{\"title\":\"dawdada\","
-							+ "\"body\":\"awdadada\"}"). 
+					header("Content-Type", "application/json").
+					// exemplu 1 --> body sub forma de string
+					//body("{\"title\":\"dawdada\","
+					//		+ "\"body\":\"awdadada\"}"). 
+					
+					// exemplu 2 --> body sub forma unui obiect json\
+					//body(requestBody.toJSONString()).
+					
+					//exemplul 3 --> body sub forma unui fisier JSON
+					body(fisierJson).
+					
 				when(). 
 					post("https://keytodorestapi.herokuapp.com/api/save").
 				then(). 
 					statusCode(200).
 					body("info",is( equalTo("Todo saved! Nice job!"))).
+					log().all().
 					extract().response();
 					
 					
@@ -31,13 +55,13 @@ public class RestExample {
 		System.out.println(response.asString());
 		System.out.println(response.asPrettyString());
 		System.out.println(response.statusCode());
-		System.out.println(response.getHeaders());
+		//System.out.println(response.getHeaders());
 
 		assertEquals(response.statusCode(), 200);
 		assertEquals(response.jsonPath().getString("info"), "Todo saved! Nice job!");
 		assertThat(response.jsonPath().getString("info"), is( equalTo("Todo saved! Nice job!")));
 		
-		assertThat(response.jsonPath().getString("info"), matchesPattern("[A-Za-z !]+"));
+		//assertThat(response.jsonPath().getString("info"), matchesPattern("[A-Za-z !]+"));
 
 		
 		System.out.println(response.jsonPath().getString("id"));
